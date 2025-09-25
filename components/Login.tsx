14a58e3f1e010cf3eb8be79613b570f8e09ee699
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-25 13:51:32 +0200

    feat(cej-9): create the Login screen & set the login proccess up

---
+++ components/Login.tsx
@@ -0,0 +1,44 @@
+import { GoogleAuth } from 'react-native-google-auth';
+import { View, Pressable, Text } from 'react-native';
+import type { LoginProps } from '../interfaces/Login';
+
+const Login = ({ setUser }: LoginProps) => {
+  async function logIn() {
+    const loginAttemptResult = await GoogleAuth.signIn();
+
+    if (loginAttemptResult.type === 'success') {
+      const idToken = loginAttemptResult.data.idToken;
+
+      const response = await fetch(
+        'https://example.com/' /* TODO: Specify API route when it is implemented */,
+        {
+          method: 'POST',
+          body: JSON.stringify({ idToken }),
+        },
+      );
+
+      if (/* response.ok UNCOMMENT WHEN THE API ROUTE IS IMPLEMENTED */ true) {
+        setUser(loginAttemptResult.data.user);
+      } else {
+        // TODO: Display error message
+      }
+    } else {
+      // TODO: Display error message
+    }
+  }
+
+  return (
+    <View>
+      <Pressable
+        onPress={logIn}
+        style={({ pressed }) => [
+          { opacity: pressed ? 0.2 : 1, marginTop: 100 },
+        ]}
+      >
+        <Text>Log in with Google</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Login;
