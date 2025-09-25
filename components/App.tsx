d549a35763346daadaed480cbf7d8c2f74cddbd3
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-25 13:44:21 +0200

    feat(cej-9): code the app initialization, during which the Google Auth is configured & the currently logged-in user (if any) is validated

---
+++ components/App.tsx
@@ -0,0 +1,65 @@
+import { User, GoogleAuth, GoogleAuthScopes } from 'react-native-google-auth';
+import { useState, useEffect } from 'react';
+import Login from './Login';
+
+const App = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    configureGoogleAuth();
+  }, []);
+
+  async function configureGoogleAuth() {
+    await GoogleAuth.configure({
+      webClientId:
+        '68777880712-79pl25jsa37639ibme1back16g29surs.apps.googleusercontent.com',
+      scopes: [GoogleAuthScopes.EMAIL],
+    });
+
+    // Check if user is already signed in
+    const currentUser: User | null = await GoogleAuth.getCurrentUser();
+
+    if (currentUser) {
+      const idToken: string = await getIdToken();
+
+      const response = await fetch(
+        'https://example.com/' /* TODO: Specify API route when it is implemented */,
+        {
+          method: 'POST',
+          body: JSON.stringify({ idToken }),
+        },
+      );
+
+      if (!response.ok) {
+        await GoogleAuth.signOut();
+        setUser(null);
+
+        // TODO: Display error message
+      }
+    }
+
+    setUser(currentUser);
+  }
+
+  async function getIdToken(): Promise<string> {
+    let tokens;
+
+    const isIdTokenExpired: boolean = await GoogleAuth.isTokenExpired();
+
+    if (isIdTokenExpired) {
+      tokens = await GoogleAuth.refreshTokens();
+    } else {
+      tokens = await GoogleAuth.getTokens();
+    }
+
+    const { idToken } = tokens;
+
+    return idToken;
+  }
+
+  return !user ? (
+    <Login setUser={setUser} />
+  ) : null /* TODO: Return the Main component */;
+};
+
+export default App;
