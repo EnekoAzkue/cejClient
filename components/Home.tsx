4e7235d96266f7c8540301a66c378252daa1cb94
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-30 13:29:53 +0200

    feat(cej-14): create the "Home" & "Settings" static screens

---
+++ components/Home.tsx
@@ -0,0 +1,12 @@
+import ScreenContainer from './ScreenContainer';
+import { Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <ScreenContainer>
+      <Text>Home Screen</Text>
+    </ScreenContainer>
+  );
+};
+
+export default Home;
---
+++ components/Settings.tsx
@@ -0,0 +1,12 @@
+import ScreenContainer from './ScreenContainer';
+import { Text } from 'react-native';
+
+const Settings = () => {
+  return (
+    <ScreenContainer>
+      <Text>Settings Screen</Text>
+    </ScreenContainer>
+  );
+};
+
+export default Settings;
