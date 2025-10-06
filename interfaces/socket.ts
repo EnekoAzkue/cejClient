8ee93d704deca9832c3f3d7c42257af80469e734
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-10-06 23:10:56 +0200

    feat(cej-52): set up & initialize Socket.IO

---
+++ interfaces/socket.ts
@@ -0,0 +1,7 @@
+// Declaration of the events used when receiving events from the server
+interface ServerToClientEvents {}
+
+// Declaration of the events used when sending events to the server
+interface ClientToServerEvents {}
+
+export type { ServerToClientEvents, ClientToServerEvents };
---
+++ socket/socket.ts
@@ -0,0 +1,14 @@
+import { io, Socket } from 'socket.io-client';
+import type {
+  ServerToClientEvents,
+  ClientToServerEvents,
+} from '../interfaces/socket';
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  'http://localhost:3000',
+  {
+    autoConnect: false,
+  },
+);
+
+export default socket;
