334c8541ffd362eb31e9c79e8f096a404d11208d
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-10-10 14:48:47 +0200

    feat(cej-55): make a QR scanner display when Istvan presses "Open camera to scan QR" in the "Scan QR" screen

--- components/roles/istvan/ScanQr.tsx
+++ components/roles/istvan/ScanQr.tsx
@@ -1,10 +1,62 @@
 import ScreenContainer from '../../ScreenContainer';
 import { ScreenBackgroundImgSrc } from '../../../constants';
+import {
+  useCameraPermission,
+  Camera,
+  useCameraDevice,
+  useCodeScanner,
+  Code,
+} from 'react-native-vision-camera';
+import { Pressable, StyleSheet } from 'react-native';
+import Text from '../../Text';
+import { useContext, useState } from 'react';
+import { ModalContext } from '../../../contexts/ModalContext';
 
 const ScanQr = () => {
-  return (
+  const [isCameraOpen, setIsCameraOpen] = useState<boolean>(false);
+
+  const setGeneralModalMessage = useContext(ModalContext);
+
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr'],
+    onCodeScanned,
+  });
+
+  function onCodeScanned(codes: Code[]) {
+    const codeValue = codes[0].value;
+    console.log(codeValue);
+    setIsCameraOpen(false);
+  }
+
+  async function handlePress() {
+    if (hasPermission) {
+      setIsCameraOpen(true);
+    } else {
+      const userGavePermission: boolean = await requestPermission();
+
+      if (!userGavePermission) {
+        setGeneralModalMessage(
+          'Istvan, you have to give permission to be able to scan QR codes.',
+        );
+      }
+    }
+  }
+
+  return isCameraOpen ? (
+    <Camera
+      device={device}
+      isActive={true}
+      style={StyleSheet.absoluteFill}
+      codeScanner={codeScanner}
+    />
+  ) : (
     <ScreenContainer backgroundImgSrc={ScreenBackgroundImgSrc.SCAN_QR}>
-      {/* TODO: Insert screen content */}
+      <Pressable onPress={handlePress}>
+        <Text>Open camera to scan QR</Text>
+      </Pressable>
     </ScreenContainer>
   );
 };
