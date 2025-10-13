2f09fa85393b2a69da23af1c38543b81a197e609
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2025-10-13 11:32:28 +0200

    fix(thet--2): make the qr display in the middle

--- src/components/QR.tsx
+++ src/components/QR.tsx
@@ -2,21 +2,34 @@ import React from 'react';
 import QRCode from 'react-native-qrcode-svg';
 import { useContext } from 'react';
 import { QRContext } from '../contexts/QRContext';
+import { View, StyleSheet } from 'react-native';
+
 
 const QR = () => {
   const logoFromFile = require('../../public/images/icon.png');
   const { email, isInside } = useContext(QRContext);
 
   return (
-    <QRCode
-      value={`email=${email}&isInside=${isInside}`}
-      size={180}
-      logo={logoFromFile}
-      logoSize={40}
-      logoBackgroundColor="white"
+    <View style={styles.container}>
 
-    />
-  );
+      <QRCode
+        value={`email=${email}&isInside=${isInside}`}
+        size={250}
+        logo={logoFromFile}
+        logoSize={40}
+      />
+    </View>
+
+);
 };
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center', 
+    alignItems: 'center',    
+  }
+});
+
 
 export default QR;
+
